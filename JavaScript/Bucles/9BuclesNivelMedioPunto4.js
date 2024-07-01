89101a11d92101416f89323f5739ca92744b8ad9
unknown <luiszyzz248@gmail.com>
2024-07-01 17:52:10 -0300

    Arreglar el punto 9 de ejercicios de bucle nivel medio

--- JavaScript/Bucles/9BuclesNivelMedioPunto4.js
+++ JavaScript/Bucles/9BuclesNivelMedioPunto4.js
@@ -1,4 +1,4 @@
-console.log("---------------NIVEL MEDIO -------Ejercicio--------3333333-----------------------")
+console.log("---------------NIVEL MEDIO -------Ejercicio--------4444444-----------------------")
 console.log("----------------COMPARANDO FECHAS----------------------")
 
 // IMPORTANTE LAS FECHAS DEBEN ESTAR EN EL FORMATO YYYY-MM-DD (4 NUMEROS -2NUMERO -2NUMEROS)
@@ -7,7 +7,7 @@ const dateLimit = new Date("1997-08-03") // Date("1997-08-03"): Este constructor
 let dateLimitTime = dateLimit.getTime() // dateLimit.getTime(): El método getTime() devuelve el número de milisegundos transcurridos desde el 1 de enero 
 //                                         de 1970 00:00:00 UTC (la época Unix) hasta la fecha representada por dateLimit. Este valor es un número 
 //                                         entero y se utiliza para facilitar la comparación de fechas.
-const dates = ["1984-02-18", "1998-02-03", "1973-07-15", "2024-10-31", "2081-09-01"]
+const dates = ["1984-02-18", "1998-02-03", "1973-07-15", "2024-10-31", "2081-09-01","1997-08-03"]
 
 
 let fechasMayoresOiguales = []
@@ -17,7 +17,7 @@ for (let i = 0; i < dates.length; i++)
         let formatoDate = new Date(dates[i]) // new Date(dates[i]): Convierte la cadena de texto dates[i] a un objeto Date.
         let formatoDateTime = formatoDate.getTime() // formatoDate.getTime(): Obtiene el timestamp (número de milisegundos desde la época Unix) 
                                                     // de la fecha actual representada por formatoDate.
-        if(dateLimitTime < formatoDateTime)
+        if(dateLimitTime < formatoDateTime || dateLimitTime == formatoDateTime)
         {
             fechasMayoresOiguales[auxPosicion] = dates[i]
             auxPosicion++ 
